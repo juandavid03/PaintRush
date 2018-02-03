1b03e38a13f84f90ef93c0309a814a1d8876d74b
juandavid03 <espinosajuan3@msn.com>
2018-02-03 13:09:37 -0500

    Botones de menus
    -Programación de los botones de menus
    -Reorganizacion de la carpeta del Git por perdida de coneccion con el repositorio

---
+++ Assets/Script/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void LoadScene(string nextScene)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/Personaje.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+
+    private GameObject personaje;
+	// Use this for initialization
+	void Start ()
+    {
+        personaje = GameObject.Find("Personaje");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        personaje.transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
+	}
+}
