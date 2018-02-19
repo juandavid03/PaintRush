4b6ca2aa2dc8171e573720a4be83ee0ecb1dddbd
juandavid03 <espinosajuan3@msn.com>
2018-02-19 11:49:09 -0500

    Cambios minimos a la escena de testeo
    -Organizacion de nombres de los assets, cambios de nombre a algunos prefabs, y mas cambios pequeños similares.

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private MenuManager manager;
+	// Use this for initialization
+	void Start ()
+    {
+        manager = GameObject.Find("MenuManager").GetComponent<MenuManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Personaje.Instance.Health <= 0)
+        {
+            Perder();
+        }
+	}
+
+    private void Perder()
+    {
+        manager.LoadScene("MainMenu");
+    }
+}
